f0106ceeac5699220eef0b8872dbf242d5e93d88
royal1shrestha <royalstha1@gmail.com>
2023-07-22 10:26:03 +0545

    Prop and State Management

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+// import Login from "./components/Login";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    // <Login />
+    <Home />
+    // const isLoggedIn = true;
+
+    // return isLoggedIn ? <Home /> : <Login />;
+  );
+}
+
+export default App;
